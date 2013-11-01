540b33e1fb7d53fcf7e972311f7bb6eca39f3263
Akshay Kr Singh <akshay_xinix@hotmail.com>
2013-11-01 15:13:05 +0530

    splash screen bug fixes | disabled menu button

--- Shield/src/main/java/com/eighlark/shield/SplashScreen.java
+++ Shield/src/main/java/com/eighlark/shield/SplashScreen.java
@@ -1,21 +1,11 @@
 package com.eighlark.shield;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.eighlark.shield.fragments.SplashFragment;
-
-public class SplashScreen extends ActionBarActivity {
+public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,33 +17,6 @@ public class SplashScreen extends ActionBarActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new SplashFragment())
-                    .commit();
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as a parent activity is specified in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
